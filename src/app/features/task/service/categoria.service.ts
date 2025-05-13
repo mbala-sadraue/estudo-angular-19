83c1ed90c325f14b42bbcae6bf9c6c1b44c21ede
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-13 08:49:10 +0100

    criar o servicºo e adicionar a base de dados

--- src/app/features/task/service/categoria.service.ts
+++ src/app/features/task/service/categoria.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable, signal } from "@angular/core";
+import { Observable, take, tap } from "rxjs";
+import { Categoria } from "../../categoria/model/categoria";
 
- @Injectable({
+@Injectable({
     providedIn: 'root'
- })
- 
-export  class CategoriaService {}
+})
+
+export class CategoriaService {
+    private readonly apiUrl = environment.apiUrl + 'categoria';
+    // constructor(private http: HttpClient) { }
+
+    private readonly httlClient = inject(HttpClient);
+
+
+    public categorias = signal<Categoria[]>([]);
+
+    getCategorias(): Observable<Categoria[]> {
+        return this.httlClient.get<Categoria[]>(this.apiUrl).pipe
+            (
+                tap((categorias) => {
+                    this.categorias.set(categorias);
+                    console.log("categorias"+categorias);
+                })
+
+            );
+    }
+}
