662fcccbcfff2363c5962b15ec19a08016f73406
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-15 16:22:10 +0100

    terminar

--- src/app/features/task/view/task-form.component/task-form.component.ts
+++ src/app/features/task/view/task-form.component/task-form.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+const MODULES = [
+]
+
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.scss'],
   standalone: true
 })
+
+
 export class TaskFormComponent implements OnInit {  
   constructor() { }
 
