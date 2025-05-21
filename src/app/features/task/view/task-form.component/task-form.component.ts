7286876a1e4a6c825e9ce5557b9ac7bb54a434f4
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-21 09:53:35 +0100

    criar oss formularios reactiva

--- src/app/features/task/view/task-form.component/task-form.component.ts
+++ src/app/features/task/view/task-form.component/task-form.component.ts
@@ -1,17 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Form, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { CategoriaService } from '../../../categoria/service/categoria.service';
 const MODULES = [
-  MatInputModule,
+  // MatInputModule,
   MatFormFieldModule,
   FormsModule,
   MatSelectModule,
-  // ReactiveFormsModule,
-  // MatIconModule,
+  ReactiveFormsModule,
+  MatIconModule,
 ]
 
 @Component({
@@ -21,18 +21,40 @@ const MODULES = [
   standalone: true,
   imports: [
     ...MODULES
-  ] 
+  ]
 })
 
 
 export class TaskFormComponent implements OnInit {
   constructor() { }
   task: string = '';
-
+  fb = inject(FormBuilder);
   categoriesServices = inject(CategoriaService)
+  formBuilder: FormGroup = new FormGroup({});
   categories = this.categoriesServices.categorias;
+  formGroup: FormGroup = new FormGroup({
+    task: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    categoryId: new FormControl('', [Validators.required]),
+  })
+
+
   ngOnInit(): void {
-    
+
+
+    this.formBuilder = this.fb.group({
+      task: ['', Validators.required, Validators.minLength(3)],
+      categoryId: ['', Validators.required],
+    });
+
+    // this.formGroup 
   }
 
+ public addTask(): any  {
+    
+    if(this.formBuilder.invalid) return false;
+
+
+    console.log(this.formBuilder.value);
+    
+}
 }
