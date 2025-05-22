f2580c7dda6b3870899fea7600ce578fb46d9b9f
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-22 14:16:44 +0100

    criar pasta para minhas tarefas

---
+++ my-task/my-tasks/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,48 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-W532FIHR.js";
+import "./chunk-2O4WY5GE.js";
+import {
+  Dir
+} from "./chunk-DTVES5Y4.js";
+import "./chunk-LRHKH4CB.js";
+import "./chunk-MCYNVD6B.js";
+import "./chunk-WLTQXWYS.js";
+import "./chunk-UPXM2XNN.js";
+import "./chunk-S35MAB2V.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
