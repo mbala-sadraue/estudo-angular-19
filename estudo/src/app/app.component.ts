3ef78e6688d29f703b5fafa92d7ab04ff7303803
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-22 16:23:44 +0100

    singnal

--- estudo/src/app/app.component.ts
+++ estudo/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { SignalComponent } from './modules/signals/signal/signal.component';
+const COMPONENTS = [
+    SignalComponent
+]
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ...COMPONENTS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
