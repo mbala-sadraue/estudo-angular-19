509aa4ff47229594b0a075f2eab4079e9ef74b31
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-24 15:44:27 +0100

    circulo de vida do angular

--- estudo/src/app/modules/components/list-item/list-item.component.ts
+++ estudo/src/app/modules/components/list-item/list-item.component.ts
@@ -1,22 +1,70 @@
-import { Component,input, model, output } from '@angular/core';
+import { Component,computed,input, model, output, ViewChild, viewChild } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+const COMPONENTS = [
+    HeaderComponent
+]
 
 @Component({
     selector: 'app-list-item',
     templateUrl: './list-item.component.html',
     styleUrls: ['./list-item.component.scss'],
-    standalone: true
+    standalone: true,
+    imports:[...COMPONENTS]
 })
 export class ListItemComponent {
+
+    header = viewChild(HeaderComponent);
+    // @ViewChild(HeaderComponent,{static:true}) _header : HeaderComponent;
     constructor() {
         // console.log('ListItemComponent initialized');
     }
+    headerTitle = computed(() => this.header()?.title)
 
+    
+    
     numero = input('',{transform: verififyValue, alias: 'value'})
-
+    
     title = input.required<string>()
-
+    
     contador = model<number>(0);
+    
+    
+    ngOnInit() {
+        console.log("OnInit 2");
+           
+    }
+    
+    ngOnChanges(): void {
+        console.log("ngOnChanges 2");
+    }
 
+    ngDocheck(): void {
+        console.log("ngDocheck 2");
+
+    }
+    ngAfterContentInit(): void {
+        console.log("ngAfterContentInit 2");
+
+    }
+    ngAfterContentChecked(): void {
+        console.log("ngAfterContentChecked 2");
+
+    }
+    ngAfterViewInit(): void{
+        console.log("ngAfterViewInit 2");
+        // console.log("header title"+ this.headerTitle);
+        console.log("header title: "+ this.headerTitle());
+        
+    }
+    ngAfterViewChecked(): void{
+        console.log("ngAfterViewChecked 2");
+    }
+
+    ngOnDestroy():void{
+        console.log("OnDesctroy 2");
+        
+    }
     clickIncrementValue = output<number>();
     incrementValue() {
         this.contador.update( (oldValue:number)=> oldValue + 1   );
@@ -27,6 +75,9 @@ export class ListItemComponent {
         this.contador.update( (oldValue) => oldValue - 1)
     }
 }
+
+
+
 
 
 function verififyValue(value: number): string {
