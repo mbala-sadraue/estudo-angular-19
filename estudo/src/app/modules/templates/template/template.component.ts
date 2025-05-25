87ac8916f1908b3189dced5037103cf84eaeef01
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-25 01:00:14 +0100

    criar component para o template

---
+++ estudo/src/app/modules/templates/template/template.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component(
+    {
+        selector:'app-template',
+        styleUrl:'template.component.scss',
+        templateUrl:'template.component.html'
+    }
+    
+)
+export class TemplateComponent {}
