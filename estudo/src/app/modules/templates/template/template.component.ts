fa9b501b345502e64bdafd479b5fa7df893b49b4
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-25 19:21:28 +0100

    trabalhar com eventos

--- estudo/src/app/modules/templates/template/template.component.ts
+++ estudo/src/app/modules/templates/template/template.component.ts
@@ -13,4 +13,24 @@ export class TemplateComponent {
     isFormValid:WritableSignal<boolean> = signal<boolean>(true)
     isFormValid2 = true
     link = 'http://www.facebook.com'
+    classBtn = 'btn'
+
+    btnHover = ['btn','btn_primary']
+    btnPrimary = {
+        btn_primary:true,
+        btn:true
+    }
+
+    onKeyup(event: KeyboardEvent){
+        // console.log("keyup => "+event.key);
+        console.log("keyup => "+event.key);
+        
+    }
+    onKeyupEnter(event: any){
+        console.log("keyup enter => "+event.key);
+    }
+    showOverlay(event: MouseEvent){
+        event.preventDefault
+        console.log("show overlay");
+    }
 }
