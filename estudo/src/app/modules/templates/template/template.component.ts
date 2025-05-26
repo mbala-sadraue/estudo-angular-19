b7ae38e78735df33620f93428c73992766d8f7f1
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-05-26 09:16:43 +0100

    lista de for

--- estudo/src/app/modules/templates/template/template.component.ts
+++ estudo/src/app/modules/templates/template/template.component.ts
@@ -1,35 +1,54 @@
-import {Component, signal, WritableSignal} from '@angular/core'
+import { Component, signal, WritableSignal } from '@angular/core'
 
 @Component(
     {
-        selector:'app-template',
-        styleUrl:'template.component.scss',
-        templateUrl:'template.component.html'
+        selector: 'app-template',
+        styleUrl: 'template.component.scss',
+        templateUrl: 'template.component.html'
     }
-    
+
 )
 export class TemplateComponent {
 
-    isFormValid:WritableSignal<boolean> = signal<boolean>(true)
+    isFormValid: WritableSignal<boolean> = signal<boolean>(true)
     isFormValid2 = true
     link = 'http://www.facebook.com'
     classBtn = 'btn'
 
-    btnHover = ['btn','btn_primary']
+    btnHover = ['btn', 'btn_primary']
     btnPrimary = {
-        btn_primary:true,
-        btn:true
+        btn_primary: true,
+        btn: true
     }
 
-    onKeyup(event: KeyboardEvent){
+    items = [
+        {
+        id: 1,
+        name: 'Item 1',
+    },
+    {
+        id: 2,
+        name: 'Item 2',
+    },
+    {
+        id: 3,
+        name: 'Item 3', 
+    },
+    {
+        id: 4,
+        name: 'Item 4',
+    },
+];
+
+    onKeyup(event: KeyboardEvent) {
         // console.log("keyup => "+event.key);
-        console.log("keyup => "+event.key);
-        
+        console.log("keyup => " + event.key);
+
     }
-    onKeyupEnter(event: any){
-        console.log("keyup enter => "+event.key);
+    onKeyupEnter(event: any) {
+        console.log("keyup enter => " + event.key);
     }
-    showOverlay(event: MouseEvent){
+    showOverlay(event: MouseEvent) {
         event.preventDefault
         console.log("show overlay");
     }
