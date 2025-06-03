499382a1f369f662f4ce538c114af44da5295510
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-06-03 10:30:54 +0100

    formuario alinhado e validação

--- estudo/src/app/modules/formularios/react-form/react-form.component.ts
+++ estudo/src/app/modules/formularios/react-form/react-form.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule,FormControl,FormGroup, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule,FormControl,FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 const MODULES = [
 
@@ -15,31 +15,63 @@ const MODULES = [
 })
 export class ReactFormComponent {
 
-  public profileForm = new FormGroup(
-    {
-      firstName: new FormControl('',[Validators.required]),
-      lastName: new FormControl(''),
-      email: new FormControl(''),
-    }
-  )
+  // public profileForm = new FormGroup(
+  //   {
+  //     firstName: new FormControl('',[Validators.required]),
+  //     lastName: new FormControl(''),
+  //     email: new FormControl(''),
+  //     address: new FormGroup({
+  //       street: new FormControl(''),
+  //       city: new FormControl(''),
+  //       state: new FormControl(''),
+  //     }),
+  //   }
+  // )
+
+  public formBuilder = inject(FormBuilder);
+  public  profileForm =  this.formBuilder.group({
+    firstName: ['', Validators.required,],
+    lastName: [''],
+    email: [''],
+    address: this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+    }),
+    alieses: this.formBuilder.array([
+      this.formBuilder.control('')])
+  });
+
+  
 
 
   onSubmit(): void{
-
-
+    
     if(this.profileForm.invalid){
       console.log('Form is invalid');
       return;
-
     }
     // Process checkout data here
     console.log('Your order has been submitted', this.profileForm.value);
     console.log('M', this.profileForm.get('firstName')?.value);
+    console.log('Alieses', this.alieses);
     
 
-    // this.profileForm.reset();
+    this.profileForm.reset();
     
 
+  }
+
+  get alieses() {
+    return this.profileForm.get('alieses') as FormArray;
+  }
+  onUpdate(): void {
+    this.profileForm.patchValue({
+      firstName: 'Elias',
+      lastName: 'Salomão',
+    })
+
+    
   }
 
 }
