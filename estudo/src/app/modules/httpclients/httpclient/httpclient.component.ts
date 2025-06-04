70933b66ba9fc81f7d886709b7f0c8fd934ddc30
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-06-04 15:58:39 +0100

    criar modulo http

---
+++ estudo/src/app/modules/httpclients/httpclient/httpclient.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-httpclient',
+  imports: [],
+  templateUrl: './httpclient.component.html',
+  styleUrl: './httpclient.component.scss'
+})
+export class HttpclientComponent {
+
+}
